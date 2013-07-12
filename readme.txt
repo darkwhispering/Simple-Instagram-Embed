dbfab14355561199124b4405c7071817454bf61c
Mattias <mattias.hedman@stopp-la.com>
2013-07-11 23:00:28 -0700

    added all files for initial release

---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Simple Instagram Embed ===
+Contributors: darkwhispering
+Tags: social, plugin, instagram, embed, instagram embed, image, video
+Requires at least: 3.0.0
+Tested up to: 3.5.2
+Stable tag: 1.0.0
+
+Past any link to a instagram picture or video in your post and the plugin replace your instagram link with the NEW Instagram Embed directly in your posts just like wordpress replace your youtube links to youtube embeds.
+
+== Description ==
+
+*BETA*
+
+Past any link to a instagram picture or video in your post and the plugin replace your instagram link with the new [Instagram Embed](http://blog.instagram.com/post/55095847329/introducing-instagram-web-embeds) directly in your posts just like wordpress replace your youtube links to youtube embeds.
+
+Settings page avaliable to select between 3 different sizes to embed.
+
+* NOTE! The Instagram is a new featured released by Instagram July 11, 2013. This plugin is in open beta, so expect that there can be issues.
+
+If you find any, please report them so they can be addressed. Thank you!
+
+More info about the new Intagram Embed: http://blog.instagram.com/post/55095847329/introducing-instagram-web-embeds
+
+== Installation ==
+
+1. Upload the zipped file to yoursite/wp-content/plugins
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Screenshots ==
+
+1. Embeded image in posts (large size)
+2. Instagram url in pasted in editor
+3. Plugin settings, select embed size
+
+== Changelog ==
+
+= 0.1.0 =
+* Initial release
---
+++ simple-instagram-embed.php
@@ -0,0 +1,88 @@
+<?php
+/*
+Plugin Name: Simple Instagram Embed
+Plugin URI: http://darkwhispering.com/wp-plugins/instagram-embed
+Description: Past any link to a instagram picture or video in your post and the plugin replace your instagram link with the NEW Instagram Embed directly in your posts just like wordpress replace your youtube links to youtube embeds.
+Author: Mattias Hedman
+Author URI: http://www.darkwhispering.com
+Version: 1.0.0
+*/
+
+add_filter('the_content', 'insta_embed', 1);
+
+function insta_embed($content) {
+
+    $size = get_option('insta_embed_size');
+    if (empty($size)) {$size = 'large';}
+
+    switch($size) {
+        case 'large':
+            $iframe_size = 'width="612" height="712"';
+            break;
+
+        case 'middle':
+            $iframe_size = 'width="480" height="580"';
+            break;
+
+        case 'small':
+            $iframe_size = 'width="350" height="450"';
+            break;
+    }
+
+    $content = preg_replace('/\/\/instagram.com\/p\/(\w+)(\/|$)/', '<iframe src="//instagram.com/p/$1/embed/" '.$iframe_size.' frameborder="0" scrolling="no" allowtransparency="true" class="instagram-embed"></iframe>', $content);
+
+    echo $content;
+}
+
+
+function insta_embed_settings() {
+        if (!current_user_can('manage_options'))  {
+            wp_die( __('You do not have sufficient permissions to access this page.') );
+        }
+
+        $size = get_option('insta_embed_size');
+        if (empty($size)) {$size = 'large';}
+
+        switch($size) {
+            case 'large':
+                $large = 'selected="selected"';
+                break;
+
+            case 'middle':
+                $middle = 'selected="selected"';
+                break;
+
+            case 'small':
+                $small = 'selected="selected"';
+                break;
+        }
+    ?>
+        <div class="wrap insta-settings">
+            <div class="icon32" id="icon-options-general"><br></div>
+            <h2>Simple Instagram Embed Settings</h2>
+            
+            <form method="post" action="options.php">
+                <?php wp_nonce_field('update-options'); ?>
+                <p>
+                    <strong><?php echo __('Choose embed size:'); ?></strong>
+                    <select name="insta_embed_size">
+                        <option value="large" <?php echo $large; ?>><?php echo __('Large (w:612 x h:712)'); ?></option>
+                        <option value="middle" <?php echo $middle; ?>><?php echo __('Middle (w:480 x h:580)'); ?></option>
+                        <option value="small" <?php echo $small; ?>><?php echo __('Small (w:350 x h:450)'); ?></option>
+                    </select>
+                </p>
+
+                <input type="hidden" name="action" value="update" />
+                <input type="hidden" name="page_options" value="insta_embed_size" />
+                <input type="submit" name="submit" value="Save" class="button-primary save" />
+            </form>
+
+        </div>
+    <?php
+    }
+
+
+function insta_embed_menu() {
+    add_submenu_page('options-general.php', 'Simple Instagram Embed Settings', 'Simple Instagram Embed', 'manage_options', 'insta-settings', 'insta_embed_settings');
+}
+add_action('admin_menu', 'insta_embed_menu');
